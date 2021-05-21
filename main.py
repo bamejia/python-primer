0ab314fc014103914c4c0153ce2f2910ef09dac2
bamejia <bamejia@udel.edu>
2021-05-21 11:10:23 -0400

    changest to main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import code_section6.app
+from code_section6.app import app
+from code_section6.db import db
 
-code_section6.app.run()
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    db.init_app(app)
