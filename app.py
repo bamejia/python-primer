6fbabb39fcbae7818e054ae34ad9e3c44e8efc4d
bamejia <bamejia@udel.edu>
2021-05-21 14:16:48 -0400

    trying to make code work with heroku

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from code_section6.security import authenticate, identity
 from code_section6.resources.user_resource import UserRegister
 from code_section6.resources.item_resource import ItemDAO, ItemListDAO
 from code_section6.resources.store_resource import StoreListResource, StoreResource
+from code_section6.resources.home_resource import Home
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -22,6 +23,7 @@ api.add_resource(ItemListDAO, "/items")
 api.add_resource(UserRegister, "/register")
 api.add_resource(StoreResource, "/store/<string:name>")
 api.add_resource(StoreListResource, "/stores")
+api.add_resource(Home, "/")
 
 
 if __name__ == "__main__":
